1bf480d51876616247cc03fd2f2b9055e77eb9e5
Matthew Gould <matt@everettgould.com>
2017-03-08 14:19:37 -0800

    order check

---
+++ ordercheck.c
@@ -0,0 +1,29 @@
+#include <pushswap.h>
+#include <libft.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int		isrevordered(t_stack *stacka, t_stack *stackb)
+{
+	int tmp;
+
+	if (stacka)
+		return (0);
+	else if (!(stackb))
+		return (0);
+	tmp = stackb->v;
+	while (stackb->nx)
+	{
+		if ((stackb->nx)->v < tmp)
+			tmp = (stackb->nx)->v;
+		else if ((stackb->nx)->v == tmp)
+			return (2);
+		else
+			return (0);
+		stackb = stackb->nx;
+	}
+	return (1);
+}
